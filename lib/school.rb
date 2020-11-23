400c6ae90eb505a6e004bc290ff491177c1b6fb2
github username <github email address>
2020-11-22 20:10:42 -0600

    commit tree 558d4d4ad9318df74aaad7fb0783e95b1210ea82

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+require 'pry'
+
+class School
+
+    attr_accessor :roster
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if !roster[grade]
+            roster[grade] = []
+        end
+        roster[grade] << name
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        sorted_roster = {}
+        @roster.each do |key, value|
+            #binding.pry
+            sorted_roster[key] = value.sort
+        end
+        sorted_roster
+    end
+
+end
